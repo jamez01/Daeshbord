b45c66349c7ae8affb5fd820324fcf18157cc9c3
James Paterni <james@blueleaf.com>
2024-12-16 00:45:55 -0500

    General improvements (#1)

--- app.rb
+++ app.rb
@@ -1,74 +1,71 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require 'yaml'
 require 'net/http'
 require 'uri'
 require 'nokogiri'
-require 'sinatra/reloader' if development?
+require 'sinatra/flash'
 require_relative 'lib/router'
-# Configuration file path
-CONFIG_FILE = 'config.yml'
 
-# Hash to store service logos
-SERVICE_LOGOS = {}
+API_KEY=ENV['TRAEFIK_DASH_API_KEY'] || ['a'..'z'].to_a.shuffle[0,8].join
 
-# Load configuration
-before do
-  content_type :html
-  @config = YAML.load_file(CONFIG_FILE) rescue {}
-  @user = @config['traefik']['username']
-  @password = @config['traefik']['password']
-  @auth = @user && @password ? "#{@user}:#{@password}@" : ""
-  @ignore = @config['ignore'] || []
-end
+class TraefikDashboardApp < Sinatra::Base
+  require 'sinatra/reloader' if development?
 
-helpers do
-  # Fetch data from the Traefik API
-  def fetch_routers
-    Router.api_url = @config['traefik']['url']
-    Router.user = @user
-    Router.password = @password
-    Router.all.reject {|router| @ignore.include?(router.name) || !router.enabled? || !router.ssl? || router.hostnames.empty? || router.hostnames.first.nil?}
+  configure :development do
+    register Sinatra::Reloader
   end
 
-  def get_icon(service,hostname,scheme)
-    puts service,hostname,scheme
-    SERVICE_LOGOS[service] ||= find_icon_url(service, hostname,scheme)
-    SERVICE_LOGOS[service]
+  enable :sessions
+  register Sinatra::Flash
+
+  # Configuration file path
+  DEFAULT_CONFIG= {
+    'traefik' => [ { 'url' => 'http://localhost:8080' } ],
+    'ignore' => [],
+    'ignore_insecure' => true
+
+  }
+  
+  CONFIG_FILE = 'config.yml'
+
+  # Hash to store service logos
+  SERVICE_LOGOS = {}
+
+  # Load configuration
+  before do
+    content_type :html
+    file_config = YAML.load_file(CONFIG_FILE) rescue {}
+    @config = DEFAULT_CONFIG.merge(file_config)
+    @config['traefik'] = [@config['traefik']] if @config['traefik'].is_a?(Hash)
+    session['api_key'] = params['api_key'] if params['api_key'] 
+    session.clear if params['logout']
   end
 
-  # Find icon URL
-  def find_icon_url(service, hostname, scheme)
-    url = "#{scheme}://#{hostname}"
-    puts "Finding icon for #{url}..."
-    response = Net::HTTP.get_response(URI(url))
-    redirect_limit = 5
-    redirect_count = 0
-    while response.is_a?(Net::HTTPRedirection)
-      redirect_count += 1
-      url = response['location']
-      url = URI.join("#{scheme}://#{hostname}", url).to_s unless url.start_with?('http')
-      puts "Following redirect to #{url}..."
-      response = Net::HTTP.get_response(URI(url))
-      break if redirect_count >= redirect_limit
+
+  get '/' do
+    @routers = fetch_routers
+    erb :dashboard
+  end
+
+  get '/ignore/:service' do |service|
+    auth!
+    @config['ignore'] << service
+    update_config(ignore: @ignore)
+    flash[:success] = "Service #{service} ignored/"
+    redirect '/'
+  end
+
+  get '/edit/:service' do |service|
+    auth!
+    Mutex.new.synchronize do
+      File.open(CONFIG_FILE, 'w') { |file| file.write(@config.to_yaml) }
     end
-
-    return "https://fakeimg.pl/100x100/575757/54e3ff?text=#{service}" unless response.is_a?(Net::HTTPOK) 
-
-    doc = Nokogiri::HTML(response.body)
-    icon_urls = doc.css('link[rel~="icon"], link[rel~="shortcut icon"], link[rel~="apple-touch-icon"]').map { |link| link['href'] }
-    icon_urls += doc.css('img').map { |img| img['src'] }
-    icon_urls.each do |icon_url|
-      icon_url = URI.join(url, icon_url).to_s unless icon_url.start_with?('http')
-      response = Net::HTTP.get_response(URI(icon_url))
-      puts "Using #{icon_url}..." if response.is_a?(Net::HTTPOK) && response.content_type.start_with?('image/')
-      return icon_url if response.is_a?(Net::HTTPOK) && response.content_type.start_with?('image/')
-    end
-    "https://fakeimg.pl/100x100/575757/54e3ff?text=#{service}"
+    flash[:success] = "Service #{service} unignored"
+    redirect '/'
   end
 end
 
-get '/' do
-  @routers = fetch_routers
-  erb :dashboard
-end
+require_relative 'lib/helpers'
+
+TraefikDashboardApp.run! if __FILE__ == $0
--- public/script.js
+++ public/script.js
@@ -1,39 +1,26 @@
-function fetchServiceIcon(hostname, scheme, elementId) {
-  const iconUrls = [
-    `${scheme}://${hostname}/android-512x512.png`,
-    `${scheme}://${hostname}/apple-icon-180x180.png`,
-    `${scheme}://${hostname}/apple-icon-152x152.png`,
-    `${scheme}://${hostname}/apple-icon-144x144.png`,
-    `${scheme}://${hostname}/ms-icon-144x144.png`,
-    `${scheme}://${hostname}/apple-icon-120x120.png`,
-    `${scheme}://${hostname}/apple-icon-114x114.png`,
-    `${scheme}://${hostname}/apple-icon-76x76.png`,
-    `${scheme}://${hostname}/apple-icon-72x72.png`,
-    `${scheme}://${hostname}/apple-icon-60x60.png`,
-    `${scheme}://${hostname}/apple-icon-57x57.png`,
-    `${scheme}://${hostname}/android-192x192.png`,
-    `${scheme}://${hostname}/apple-touch-icon.png`,
-    `${scheme}://${hostname}/favicon-16x16.png`,
-    `${scheme}://${hostname}/favicon.ico`
-  ];
-
-  const iconElement = document.getElementById(elementId);
-
-  function tryNextIcon(index) {
-    if (index >= iconUrls.length) {
-      iconElement.innerHTML = '<p>No icon found</p>';
-      return;
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    var flash = document.querySelector('.flash');
+    if (flash) {
+      flash.classList.add('fade-out');
+      setTimeout(function() {
+        flash.style.display = 'none';
+      }, 500);
     }
+  }, 4000);
 
-    const img = new Image();
-    img.src = iconUrls[index];
-    img.onload = () => {
-      iconElement.innerHTML = `<img src="${img.src}" alt="Service Icon">`;
-    };
-    img.onerror = () => {
-      tryNextIcon(index + 1);
-    };
-  }
+  document.querySelectorAll('.dropdown-icon').forEach(function(icon) {
+    icon.addEventListener('click', function() {
+      var dropdownContent = this.nextElementSibling;
+      dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    });
+  });
 
-  tryNextIcon(0);
-}
+  window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropdown-icon')) {
+      document.querySelectorAll('.dropdown-content').forEach(function(content) {
+        content.style.display = 'none';
+      });
+    }
+  });
+});
